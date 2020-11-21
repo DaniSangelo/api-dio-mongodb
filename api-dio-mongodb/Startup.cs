587b57e505c9e2637b7eac049769d459077fa9f1
Daniel Sangelo <danielsangelo@hotmail.com>
2020-11-21 15:21:10 -0300

    removing weatherforecast.cs and weatherforecastcontroller.cs files

--- api-dio-mongodb/Startup.cs
+++ api-dio-mongodb/Startup.cs
@@ -32,16 +32,10 @@ namespace api_dio_mongodb
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
+         
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
